2723322bc3e122b8ef11f52ffd93bbdbbd244ff5
Gabriel Burgos <944868+gburgose@users.noreply.github.com>
2026-05-21 18:09:40 -0400

    fix(ecosystem): update cwd and environment configuration for production

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -3,19 +3,15 @@ module.exports = {
     {
       name: 'konbini-api',
       script: 'dist/src/main.js',
-      cwd: '/home/forge/konbini/apps/api',
+      cwd: '/home/forge/tu-dominio.cl/current',
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
-      env: {
+      env_production: {
         NODE_ENV: 'production',
-        PORT: 3333,
       },
-      error_file: '/home/forge/.pm2/logs/konbini-api-error.log',
-      out_file: '/home/forge/.pm2/logs/konbini-api-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
   ],
 };

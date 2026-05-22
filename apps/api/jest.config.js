9cc851206ad11e57ee9765657383228407217980
Gabriel Burgos <944868+gburgose@users.noreply.github.com>
2026-05-22 15:47:42 -0400

    chore(07-01): configure Jest in the NestJS API

---
+++ apps/api/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.(spec|e2e-spec)\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {},
+};
